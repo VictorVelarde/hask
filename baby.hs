7eab9d270d076887d0ada19d32b22a2cd66fcf1c
Víctor Velarde <victor.velarde@gmail.com>
2017-02-26 18:15:37 +0100

    End of the first lesson 'Starting'

--- baby.hs
+++ baby.hs
@@ -2,5 +2,9 @@ doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else x * 2
 toOneHundred x = [x..100]
-evenFrom x = [y | y <-[x..100], y `mod` 2 /= 0]
+oddFrom x = [y | y <-[x..100], y `mod` 2 /= 0]
 ifYouAreHappy x = if x == "Happy" then "Clap your hands" else "ups"
+triangles = [(a, b, c) | c <-[1..10], b <-[1..10], a <-[1..10]]
+someTriangles = [(a, b, c) | c<-[1..10], b <-[1..10], a <-[1..10], a + b + c == 24]
+rightTriangles = [(a, b, c) | c<-[1..10], b <-[1..10], a <-[1..10], a^2 + b^2 == c^2, a + b + c == 24]
+
